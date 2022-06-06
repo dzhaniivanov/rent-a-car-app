77a123523f0062ab43e0bc1bde40dae5e90753a6
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-06-06 13:07:16 +0300

    added assets,start layout with navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
-import './App.css';
+import styled from "styled-components";
+import tw from "twin.macro";
+import "./App.css";
+import { HomePage } from "./app/containers/HomePage";
+
+const AppContainer = styled.div`
+  ${tw`
+  w-full
+  h-full
+  flex
+  flex-col
+`};
+`;
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-     edfgvb
-    </div>
+    <AppContainer>
+      <HomePage />
+    </AppContainer>
   );
 }
 
