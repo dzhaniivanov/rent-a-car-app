a4c924f7e9dbf9806a973f3bfba924d656554e1d
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-06-06 18:23:05 +0300

    buttons section added

--- client/src/app/containers/HomePage/topSection/topSection.tsx
+++ client/src/app/containers/HomePage/topSection/topSection.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 
-import MclarenCarImg from "../../../../assets/images/mclaren-orange-big.png"
+import MclarenCarImg from "../../../../assets/images/mclaren-orange-big.png";
 import BlobImg from "../../../../assets/images/blob.svg";
+import { SCREENS } from "../../../components/responsive";
+import { Button } from "../../../components/button";
 
 const TopSectionContainer = styled.div`
   min-height: 400px;
@@ -83,6 +85,30 @@ const BlobContainer = styled.div`
     height: auto;
     max-height: max-content;
   }
+
+  @media (min-width: ${SCREENS.sm}) {
+    width: 40em;
+    max-height: 10em;
+    right: -9em;
+    top: -16em;
+    transform: rotate(-25deg);
+  }
+
+  @media (min-width: ${SCREENS.lg}) {
+    width: 50em;
+    max-height: 10em;
+    right: -7em;
+    top: -15em;
+    transform: rotate(-30deg);
+  }
+
+  @media (min-width: ${SCREENS.xl}) {
+    width: 70em;
+    max-height: 10em;
+    right: -15em;
+    top: -25em;
+    transform: rotate(-20deg);
+  }
 `;
 
 const StandaloneCar = styled.div`
@@ -97,6 +123,24 @@ const StandaloneCar = styled.div`
     height: 100%;
     max-width: fit-content;
   }
+
+  @media (min-width: ${SCREENS.sm}) {
+    height: 16em;
+    right: -6em;
+    top: -6em;
+  }
+
+  @media (min-width: ${SCREENS.lg}) {
+    height: 21em;
+    right: -8em;
+    top: -5em;
+  }
+
+  @media (min-width: ${SCREENS.xl}) {
+    height: 30em;
+    right: -13em;
+    top: -9em;
+  }
 `;
 
 export function TopSection() {
@@ -109,6 +153,8 @@ export function TopSection() {
           at the best price for you and get the best quality cars for as long as
           you like
         </Description>
+        <Button text="Book Your Ride " />
+        <Button theme="filled" text="Sell Your Car" />
       </LeftContainer>
       <RightContainer>
         <BlobContainer>
